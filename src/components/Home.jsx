2715ed528741bc1498db4fed61b09429dd9260a6
tajwararik <tajwar.arik.10@gmail.com>
2025-07-18 21:11:06 +0600

    Created Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,20 @@
+import NavButton from "./NavButton";
+import styles from "../styles/Home.module.css";
+
+function Home() {
+  return (
+    <>
+      <h1>
+        Welcome to <span>ReactCart</span>
+      </h1>
+      <p>Shopping made smarter.</p>
+      <p>Explore. Add. Checkout — all in one lightning-fast experience.</p>
+
+      <div className={styles.shop}>
+        <NavButton>Shop</NavButton>
+      </div>
+    </>
+  );
+}
+
+export default Home;
