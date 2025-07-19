afcaee75609b9d51b520a3e9c7d6bdf67ca5b497
tajwararik <tajwar.arik.10@gmail.com>
2025-07-19 19:26:37 +0600

    Fetched data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.in/api/products?limit=20")
+      .then((response) => response.json())
+      .then((data) => setProducts(data.products));
+  }, []);
+
   return (
     <>
       <header>
