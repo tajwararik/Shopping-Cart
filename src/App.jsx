5d0cdd39314140c8691cd1c52bd74345d6aa4970
tajwararik <tajwar.arik.10@gmail.com>
2025-07-21 04:30:44 +0600

    Created isShopClicked state and replaced the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Shop from "./components/Shop";
+import About from "./components/About";
 import "./App.css";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [isShopClicked, setIsShopClicked] = useState(false);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.in/api/products?limit=20")
+    fetch(
+      "https://dummyjson.com/products/category/mobile-accessories?select=title,price,images"
+    )
       .then((response) => response.json())
       .then((data) => setProducts(data.products));
   }, []);
+
+  const handleClick = () => {
+    setIsShopClicked((prevState) => !prevState);
+  };
 
   return (
     <>
@@ -19,7 +28,8 @@ function App() {
       </header>
 
       <main>
-        <Home />
+        {!isShopClicked && <Home handleClick={handleClick} />}
+        {isShopClicked && <Shop products={products} />}
       </main>
     </>
   );
