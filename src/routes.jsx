01da43f7a709d063f4ca0a8e031a053be8ef5021
tajwararik <tajwar.arik.10@gmail.com>
2025-07-24 05:16:00 +0600

    Created path for checkout

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import App from "./App.jsx";
 import Home from "./components/Home.jsx";
 import Shop from "./components/Shop.jsx";
 import About from "./components/About.jsx";
+import Checkout from "./components/Checkout.jsx";
 
 const routes = [
   {
@@ -11,6 +12,7 @@ const routes = [
       { index: true, element: <Home /> },
       { path: "shop", element: <Shop /> },
       { path: "about", element: <About /> },
+      { path: "checkout", element: <Checkout /> },
     ],
   },
 ];
