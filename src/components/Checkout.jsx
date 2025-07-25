0988ff7befca66065bb797458c414e5185fda525
tajwararik <tajwar.arik.10@gmail.com>
2025-07-26 02:08:59 +0600

    Created deleteCart function

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -3,7 +3,7 @@ import { RiDeleteBin6Fill } from "react-icons/ri";
 import styles from "../styles/Checkout.module.css";
 
 function Checkout() {
-  const { cartList } = useOutletContext();
+  const { cartList, updateCartList } = useOutletContext();
 
   const uniqueCarts = {};
 
@@ -19,16 +19,24 @@ function Checkout() {
 
   const uniqueCartList = Object.values(uniqueCarts);
 
-  const cartListElements = uniqueCartList.map((cart, index) => {
+  const deleteCart = (cart) => {
+    const newCartList = cartList.filter((item) => item.id !== cart.id);
+    updateCartList(newCartList);
+  };
+
+  const cartListElements = uniqueCartList.map((cart) => {
     return (
-      <div key={index} className={styles.cart}>
+      <div key={cart.id} className={styles.cart}>
         <img src={cart.images[0]} alt={cart.title} />
         <div>
           <p>{cart.title}</p>
           <p>${cart.price}</p>
         </div>
         <div className={styles.buttons}>
-          <RiDeleteBin6Fill className={styles.deleteButton} />
+          <RiDeleteBin6Fill
+            className={styles.deleteButton}
+            onClick={() => deleteCart(cart)}
+          />
           <div>
             <button>+</button>
             <span>{cart.quantity}</span>
@@ -39,8 +47,8 @@ function Checkout() {
     );
   });
 
-  const total = cartList.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.price,
+  const total = uniqueCartList.reduce(
+    (accumulator, cart) => accumulator + cart.price * cart.quantity,
     0
   );
 
